b80772a2f144897db124e73e2b7c21aab932b64c
Oshay Jackson <oshay@OSHAY.local>
2023-12-19 12:02:56 -0800

    created a blank signup-page.css file in prep for front end. just committing the changes made on signUP.jsx

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,39 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 function SignUpPage() {
-  return (
-    <div className="signUp">
-      <label>Email: </label>
-      <input
-        required
-        type="text"
-        name="email"
-        placeholder="Enter email: email@example.com"
-        onChange={this.handleInputChange}
-      />
-      <label>Username: </label>
-      <input
-        required
-        type="text"
-        name="username"
-        placeholder="Create username"
-        onChange={this.handleInputChange}
-      />
-      <label>Password: </label>
-      <input
-        required
-        type="text"
-        name="password"
-        placeholder="Create password"
-        onChange={this.handleInputChange}
-      />
-      <button required onClick={this.postNewUserSignUp}>
-        Save and Submit
-      </button>
-    </div>
-  );
+  const [formState, setFormState] = useState({ email: "", username: "", password: "" });
+  const handleChange = (e) => {
+    const { email, username, password } = e.target.value;
+    setFormState({ ...formState, email: email, username: username, password: password });
+    const handleFormSubmit = async (e) => {
+      e.preventDefault();
+
+      const data = {
+        email: formState.email,
+        username: formState.username,
+        password: formState.password,
+      };
+      console.table(data);
+
+      return (
+        <div className="signUp">
+          <label>Email: </label>
+          <input
+            required
+            type="text"
+            name="email"
+            placeholder="Enter email: email@example.com"
+            onChange={this.handleInputChange}
+          />
+          <label>Username: </label>
+          <input
+            required
+            type="text"
+            name="username"
+            placeholder="Create username"
+            onChange={this.handleInputChange}
+          />
+          <label>Password: </label>
+          <input
+            required
+            type="text"
+            name="password"
+            placeholder="Create password"
+            onChange={this.handleInputChange}
+          />
+          <button required onClick={this.postNewUserSignUp}>
+            Save and Submit
+          </button>
+        </div>
+      );
+    };
+  };
 }
 
 export default SignUpPage;

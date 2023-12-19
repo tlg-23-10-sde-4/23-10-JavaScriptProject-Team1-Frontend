36a5b443e72dd2bab9605e8f9fdc08a06de8b8f0
Oshay Jackson <oshay@OSHAY.local>
2023-12-18 16:09:05 -0800

    created Navbar.css, Dir: assets/ dir: css && dir: images

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "./assets/css/Navbar.css";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-    <Router>  
+    <Router>
       <Routes>
         {/* Routes to the pages we want to show */}
-        <Route path="/" element={ <Home  /> } />
-        <Route path="/login" element={ <Home  /> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Home />} />
       </Routes>
     </Router>
   );

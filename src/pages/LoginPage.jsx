6b4244298bb5a539dba3d2a7e4f86c4532912e41
jaytaylor-dev <javaris.taylor@tlgcohort.com>
2023-12-18 16:47:10 -0800

    Login page structure created and Routes added to Home

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Styles from "./login.css"
+
+function LoginPage() {
+    return (
+        <>
+            <div className="card-container">
+                <div className="card" style={{width: "18rem"}}>
+                <div className="mb-3 row">
+                        <label for="staticEmail" className="col col-form-label">Email</label>
+                        <div className="col-sm-10">
+                        <input type="text" required className="form-control" id="staticEmail" placeholder="email@example.com" />
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="inputPassword" className="col col-form-label">Password</label>
+                        <div className="col-sm-10">
+                        <input type="password" className="form-control" id="inputPassword" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginPage;

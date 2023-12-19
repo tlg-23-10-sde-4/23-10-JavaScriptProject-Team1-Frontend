5834b14d68a4618665317add7f97c6fc0826328d
Oshay Jackson <oshay@OSHAY.local>
2023-12-18 19:17:20 -0800

    added buttons to the navbar, fixed the animation and ready to move on for the time being

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,20 +7,37 @@ const Navbar = () => {
     <header className="Navbar" id="Navbar-component-main">
       <nav className="navbar sticky-top bg-body-tertiary">
         <div className="container-fluid">
+          <div class="wave"></div>
+
           <span className="navbar-brand mb-0 h1">
-            <h1>Kon-Bon Gaming</h1>
+            <h1 id="Navbar-title">Kon-Bon Gaming</h1>
+            <div class="wave"></div>
           </span>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/game-catalog">Game Catalog</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
+
+          <div class="row row-cols-3">
+            <div class="col">
+              <button type="button">
+                {" "}
+                <Link to="/">Home</Link>
+              </button>
+            </div>
+            <div class="col">
+              <button type="button">
+                {" "}
+                <Link to="/game-catalog">Game Catalog</Link>
+              </button>
+            </div>
+            <div class="col">
+              <button type="button">
+                {" "}
+                <Link to="/login">Login</Link>
+              </button>
+
+              <div class="wave"></div>
+            </div>
+          </div>
+
+          <div class="wave"></div>
         </div>
       </nav>
     </header>

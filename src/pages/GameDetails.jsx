521d542b1ba5b64ca7e40a2f5c163c968899576b
jaytaylor-dev <javaris.taylor@tlgcohort.com>
2023-12-21 09:42:41 -0800

    GameDetails staging

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -4,8 +4,30 @@ import Card from "react-bootstrap/Card";
 import { ListGroup } from "react-bootstrap";
 
 const GameDetails = (props) => {
-  const game = props.data.game_id;
-  console.log(game);
+  // const game = props.data.game_id;
+  // console.log(game);
+
+  // You need these two together (Move these to the GameDetails Page)
+  // Call the getGameById in a 'useEffect' hook.
+  // Create a local 'isLoading' state to show loading screen while we get the data
+  const [gameData, setGameData] = useState({});
+  const getGameById = async (gameId) => {
+    const url = `http://localhost:3001/api/gameById/${gameId}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    const resData = await response.json();
+    if (response.status === 200) {
+      setGameData(resData);
+    } else {
+      alert(`${resData.message}`); // Change this to toast.error
+      setTimeout(() => {
+        window.location.replace("/GameCatalog");
+      }, 2000);
+    }
+  };
 
   // const { gameId } = useParams();
   return (
@@ -15,11 +37,6 @@ const GameDetails = (props) => {
       </div>
 
       {/* different idea for page styling
-      
-      
-      
-      
-      
       
       
       */}

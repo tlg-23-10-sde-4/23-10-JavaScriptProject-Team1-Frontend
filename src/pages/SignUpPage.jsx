e4671ec95bd503618f49a8b1d27733f7d5955cb1
Oshay Jackson <oshay@OSHAY.local>
2023-12-21 09:11:34 -0800

    updating

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,18 +2,18 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 
-function SignUpPage() {
+const SignUpPage = () => {
   const [formState, setFormState] = useState({ email: "", username: "", password: "" });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormState({ ...formState, [name]: value });
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const postNewUserSignUp = async (e) => {
-    e.preventDefault(e);
+  const postNewUserSignUp = async () => {
+    console.log("Entering POST phase for post signup user");
     const data = {
       email: formState.email,
       username: formState.username,
@@ -23,90 +23,91 @@ function SignUpPage() {
     try {
       setLoading(true);
 
-      const response = await fetch("http://localhost:3001/auth/signup", {
+      const response = await fetch("http://localhost:3001/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
+
       const res = await response.json();
+
       if (!response.ok) {
-        if (response.status === 400) {
-          toast.error(`${res.message}`, {
-            position: toast.POSITION.TOP_CENTER,
-            draggable: false,
-          });
-        } else if (response.status === 401) {
+        toast.error(`${res.message}`, {
+          position: toast.POSITION.TOP_CENTER,
+          draggable: false,
+        });
+      } else {
+        // Check for response with no content (204)
+        if (!response.no_content) {
           toast.error(`${res.message}`, {
             position: toast.POSITION.TOP_CENTER,
             draggable: false,
           });
         } else {
-          toast.error(`${res.message}`, {
+          toast.success(`${res.message}`, {
             position: toast.POSITION.TOP_CENTER,
             draggable: false,
           });
         }
       }
-
-      // Check for response with no content (204)
-      if (!response.no_content) {
-        toast.error(`${res.message}`, {
-          position: toast.POSITION.TOP_CENTER,
-          draggable: false,
-        });
-      }
-
-      toast.success(`${res.message}`, {
-        position: toast.POSITION.TOP_CENTER,
-        draggable: false,
-      });
-      // Handle success for signing up
     } catch (error) {
-      toast.error(`${error}`, {
+      console.error("Error during signup:", error);
+      toast.error("Error during signup. Please try again.", {
         position: toast.POSITION.TOP_CENTER,
         draggable: false,
       });
       setError(error.message);
     } finally {
       setLoading(false);
+      window.location.replace("/");
     }
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    postNewUserSignUp();
+  };
+
   return (
-    <div>
-      <form onSubmit={postNewUserSignUp}>
-        <label>Email:</label>
+    <div className="signUp">
+      <form onSubmit={onSubmit}>
+        <label>Email: </label>
         <input
           required
           type="text"
           name="email"
+          id="email"
           placeholder="Enter email: email@example.com"
           onChange={handleInputChange}
         />
-        <label>Username:</label>
+        <label>Username: </label>
         <input
           required
           type="text"
           name="username"
+          id="username"
           placeholder="Create username"
           onChange={handleInputChange}
         />
-        <label>Password:</label>
+        <label>Password: </label>
         <input
           required
           type="password"
           name="password"
+          id="password"
           placeholder="Create password"
           onChange={handleInputChange}
         />
         <button disabled={loading} type="submit">
           {loading ? "Signing up..." : "Save & Submit"}
         </button>
+
+        {error && <div className="error-message">{error}</div>}
       </form>
     </div>
   );
-}
+};
 
 export default SignUpPage;
